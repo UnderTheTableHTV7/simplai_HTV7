409833a23c968d55083dcee8e047f8208bea9b61
Shubhaankar Sharma <66492212+Shubhaankar-Sharma@users.noreply.github.com>
2022-10-16 06:53:11 -0400

    lfg

--- ai-api/main.py
+++ ai-api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Body
 # from ./simplify import simplify_text
 from simplify import simplify_text
 from simplify_cohere import simplify_text_cohere
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    access_control=False
 )
 
 @app.get("/")
@@ -19,20 +20,19 @@ async def root():
     return "."
 
 @app.post("/simplify/")
-async def simplify(request: Request):
-    data = await request.json()
-    return {"simplified_text": simplify_text(data['input_text'])}
+def simplify(payload: dict = Body(...)):
+    input_text = payload['input_text']
+    return {"simplified_text": simplify_text(input_text)}
 
 @app.post("/simplify_cohere/")
-async def simplify_cohere(request: Request):
-    data = await request.json()
-    return {"simplified_text": simplify_text_cohere(data['input_text'])}
+def simplify_cohere(payload: dict = Body(...)):
+    input_text = payload['input_text']
+    return {"simplified_text": simplify_text_cohere(input_text)}
 
 
 @app.post("/simplify_nested/")
-async def simplify_nested(request: Request):
-    data = await request.json()
-    input_text = data['input_text']
+def simplify_nested(payload: dict = Body(...)):
+    input_text = payload['input_text']
     if len(input_text.split(' ')) > 100:
         return {"simplified_text": simplify_text_cohere(input_text)}
     else:
@@ -40,23 +40,23 @@ async def simplify_nested(request: Request):
 
 
 
-# def init_webhooks(base_url):
-#     # Update inbound traffic via APIs to use the public-facing ngrok URL
-#     pass
+def init_webhooks(base_url):
+    # Update inbound traffic via APIs to use the public-facing ngrok URL
+    pass
 
-# # pyngrok should only ever be installed or initialized in a dev environment when this flag is set
-# from pyngrok import ngrok
-# import sys
-# from fastapi.logger import logger
+# pyngrok should only ever be installed or initialized in a dev environment when this flag is set
+from pyngrok import ngrok
+import sys
+from fastapi.logger import logger
 
-# # Get the dev server port (defaults to 8000 for Uvicorn, can be overridden with `--port`
-# # when starting the server
-# port = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else 8000
+# Get the dev server port (defaults to 8000 for Uvicorn, can be overridden with `--port`
+# when starting the server
+port = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else 8000
 
-# # Open a ngrok tunnel to the dev server
-# public_url = ngrok.connect(port).public_url
-# # logger.info("ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
-# print(public_url)
-# # Update any base URLs or webhooks to use the public ngrok URL
-# # settings.BASE_URL = public_url
-# init_webhooks(public_url)
+# Open a ngrok tunnel to the dev server
+public_url = ngrok.connect(port).public_url
+# logger.info("ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
+print(public_url)
+# Update any base URLs or webhooks to use the public ngrok URL
+# settings.BASE_URL = public_url
+init_webhooks(public_url)
