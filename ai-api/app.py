fa6f555a2c412df94e47ac3a9d851b3b6b01b149
Shubhaankar Sharma <66492212+Shubhaankar-Sharma@users.noreply.github.com>
2022-10-15 22:36:38 -0400

    cohere shit
    cohere shit

--- ai-api/app.py
+++ ai-api/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 # from ./simplify import simplify_text
 from simplify import simplify_text
+from simplify_cohere import simplify_text_cohere
 
 app =  FastAPI()
 
@@ -11,3 +12,12 @@ async def root():
 @app.post("/simplify/")
 def simplify(input_text: str):
     return {"simplified_text": simplify_text(input_text)}
+
+@app.post("/simplify_cohere/")
+def simplify_cohere(input_text: str):
+    return {"simplified_text": simplify_text_cohere(input_text)}
+
+
+@app.post("/simplify_nested/")
+def simplify_nested(input_text: str):
+    return {"simplified_text": simplify_text(simplify_text_cohere(input_text))}
