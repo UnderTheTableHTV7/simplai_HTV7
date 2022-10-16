798e8ad5e8a6f11cf9e87b3d09ec9b8e6f24eb5f
Shubhaankar Sharma <66492212+Shubhaankar-Sharma@users.noreply.github.com>
2022-10-16 05:57:56 -0400

    cors

--- ai-api/main.py
+++ ai-api/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, Request
 # from ./simplify import simplify_text
 from simplify import simplify_text
 from simplify_cohere import simplify_text_cohere
+from fastapi.middleware.cors import CORSMiddleware
 
 app =  FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
